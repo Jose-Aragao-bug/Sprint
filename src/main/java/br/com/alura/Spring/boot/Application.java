b18aa6972dd9c3ff87aae946e70175f64c9d6b72
Jose-Aragao-bug <tddtide@gmail.com>
2024-12-07 10:59:51 -0300

    primeiro commit

---
+++ src/main/java/br/com/alura/Spring/boot/Application.java
@@ -0,0 +1,26 @@
+package br.com.alura.Spring.boot;
+
+import br.com.alura.Spring.boot.model.DadosSerie;
+import br.com.alura.Spring.boot.service.ConsumoApi;
+import br.com.alura.Spring.boot.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
